7923ede141c0a5f743e0dc5975917304cfe1fd94
zaidun <35617206+zaidun@users.noreply.github.com>
2020-04-04 19:09:43 +0200

    css

--- js/components/details.js
+++ js/components/details.js
@@ -1,7 +1,69 @@
 const Details = {
-        template: `
+        template: 
+        `
+        <div>
+        <header>
+    <div class="container">
+        <div>
+            <div class="div_logo">
+                <img class="logo" src="./images/Restez_en_ligne.png" alt="Logo">
+            </div>
+        </div>
+        
+    </div>
+
+</header>
+<main>
+        <div class="col-12 col-md-6 col-lg-6">
+                <h1 class="accueil">Accueil > Appeler avec Skype </h1> <br>
+        </div>
         
 
+        <div class="col-12 col-md-6 col-lg-6">
+                <h1 class = "AppelerTwo" >Appeler avec Skype </h1><br>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <img class="Skype_img" src=" images/RE2WZc5.png" alt="Skype">
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <span class="paragraphe">Skype est un logiciel qui permet de 
+                discuter à travers le monde. Des millions d'individus et 
+                d'entreprises utilisent Skype pour <p class="paragrapheInside"> effectuer des appels 
+                vidéo et des appels vocaux gratuits</p> avec une seule personne. 
+                Vous pouvez utiliser Skype sur l'appareil qui vous convient le plus : 
+                votre mobile, votre ordinateur ou votre tablette.
+                Skype peut être téléchargé gratuitement et il est simple à utiliser.
+                </span>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <div class="telecharge"> 
+                        <p class="telecharger">Télécharger skype</p>
+                        <p class="SkypeUrl">Télécharger Skype pour ordinateur de bureau ou pour mobile </p>
+                </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <div class="compte "> 
+                        <p class="créerCompte">Se créer un compte gratuitement</p>
+                        <p class="créerSkype">Comment créer un compte Skype </p>
+                </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <div class="connect1"> 
+                        <p class="connect2">Se connecter à Skype</p>
+                        <p class="connect3">Vous connecter à Skype à l'aide d'un pseudo Skype, d'une adresse e-mail ou d'un numéro de téléphone </p>
+                </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6">
+                <div class="ajoute1 "> 
+                        <p class="ajoute2">Ajouter un contact dans Skype</p>
+                        <p class="ajoute3">Rechercher et ajouter un contact dans Skype</p>
+                </div>
+        </div>
+</main>
+<footer class="fotter2">
+</footer>
+
+</div>
 `
 ,
 data(){
