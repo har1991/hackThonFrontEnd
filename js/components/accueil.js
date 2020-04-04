71cbbf0047a4c13f55c0e74bcd180d876b0dc4e8
zaidun <35617206+zaidun@users.noreply.github.com>
2020-04-04 11:02:53 +0200

    Update accueil.js

--- js/components/accueil.js
+++ js/components/accueil.js
@@ -1,6 +1,16 @@
 const accueil = {
     template: `
-    <h1> Hello all </h1>
+    <div>
+            <div class="logo">Restez<br>en ligne</div>
+        </div>
+        <div class="h_main_text">
+            <span class="h_title">Restez en ligne</span> est votre
+            allié pour apprendre à utiliser internet et les nouvelles technologies
+            en toute simplicité!
+        </div>
+        <div>
+            <img class="h_img" src="./images/hero.png" alt="">
+        </div>
 
 `
 }
