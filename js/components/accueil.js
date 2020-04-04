fc7c191048655cac7ea22ba6ebaf02496bd3d1ac
zaidun <35617206+zaidun@users.noreply.github.com>
2020-04-04 11:25:11 +0200

    Update accueil.js

--- js/components/accueil.js
+++ js/components/accueil.js
@@ -3,11 +3,7 @@ const accueil = {
     <div>
             <div class="logo"><h1>Restez<br>en ligne </h1></div>
         </div>
-        <div class="h_main_text">
-            <span class="h_title"><h3>Restez en ligne est votre
-            allié pour apprendre à utiliser internet et les nouvelles technologies
-            en toute simplicité!</h3></span> 
-        </div>
+        
        
 
 `
